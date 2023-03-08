95c0d851dc3891b72c972477e6f8fe21704d0539
TimebotsGithubno2 <githubtimebots52752@gmail.com>
2023-03-07 19:14:07 -0600

    Rotation Matrix
    YIPPEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE

--- bobot/Assets/glop.cs
+++ bobot/Assets/glop.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class glop : MonoBehaviour
@@ -10,84 +11,124 @@ public class glop : MonoBehaviour
 
     Vector3 orig;
 
-    private void Awake()
-    {
-        orig = transform.eulerAngles;
-    }
-
     void Update()
     {
         Debug.DrawLine(transform.position, transform.position + (transform.forward * 3), Color.green);
 
-        Vector3 worldRot = transform.eulerAngles;
-        pitch = worldRot.x;
-        roll = worldRot.z;
+        Matrix posMatrix = Matrix.PositionMatrix(Vector3.forward);
+        Matrix rotationMatrix = Matrix.RotationMatrix(transform.eulerAngles * Mathf.Deg2Rad);
+        Matrix rotated = rotationMatrix * posMatrix;
 
-        float[] robotRot = new float[] { transform.forward.z, transform.forward.y, transform.forward.x };
-        Vector3 glerp = new Vector3(robotRot[0], robotRot[1], robotRot[2]);
-
-        Matrix matrix = new Matrix(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z);
-        float[] hehe = matrixMultiply(matrix.generalRotationMatrixExtrinsic, robotRot);
-        Vector3 e = new Vector3(hehe[0], 0, hehe[2]);
-        Debug.Log("robotRot: " + glerp + " | e: " + e);
-
-
-        Debug.DrawLine(transform.position, transform.position + (Quaternion.Inverse(transform.rotation) * transform.forward) * 3, Color.black);
+        Debug.DrawLine(transform.position, transform.position + (Vector3)rotated * 3, Color.red);
     }
 
-    float[] balls = new float[] { 1f, 1f, 1f };
-
-    public static float[] matrixMultiply(float[][] firstMatrix, float[] secondMatrix)
-    {
-        float[] newMatrix = new float[firstMatrix.GetLength(0)];
-        if (firstMatrix.GetLength(0) == secondMatrix.GetLength(0))
-        {
-            for (int i = 0; i < 3; i++)
-            {
-                float currentRow = 0;
-                for (int j = 0; j < 3; j++)
-                {
-                    currentRow += firstMatrix[i][j] * secondMatrix[j];
-                }
-                newMatrix[i] = currentRow;
-            }
-
-        }
-        return newMatrix;
-    }
 }
 
 public class Matrix
 {
-    public float[][] generalRotationMatrixIntrinsic;
-    public float[][] generalRotationMatrixExtrinsic;
+    public float[][] matrix;
 
-    public float alpha;
-    public float beta;
-    public float gamma;
-
-    public Matrix(float x, float y, float z)
+    public Matrix(float[][] matrix)
     {
-        alpha = y;
-        beta = x;
-        gamma = z;
-
-        CalculateMatrices();
+        this.matrix = matrix;
     }
 
-    public void CalculateMatrices()
+    public static Matrix operator *(Matrix one, Matrix two)
     {
-        generalRotationMatrixIntrinsic = new float[][] {new float[]{Mathf.Cos(alpha) * Mathf.Cos(beta), Mathf.Cos(alpha) * Mathf.Sin(beta) * Mathf.Sin(gamma) - Mathf.Sin(alpha) * Mathf.Cos(gamma), Mathf.Cos(alpha) * Mathf.Sin(beta) * Mathf.Cos(gamma) + Mathf.Sin(alpha) * Mathf.Sin(gamma)},
-    new float[]{Mathf.Sin(alpha) * Mathf.Cos(beta), Mathf.Sin(alpha) * Mathf.Sin(beta) * Mathf.Sin(gamma) + Mathf.Cos(alpha) * Mathf.Cos(gamma), Mathf.Sin(alpha) * Mathf.Sin(beta) * Mathf.Cos(gamma) - Mathf.Cos(alpha) * Mathf.Sin(gamma)},
-    new float[]{-Mathf.Sin(beta), Mathf.Cos(beta) * Mathf.Sin(gamma), Mathf.Cos(beta) * Mathf.Cos(gamma)}};
+        Matrix newMatrix = new Matrix(ArrayHelper.FloatArray(one.matrix.Length, two.matrix[0].Length));
 
-    generalRotationMatrixExtrinsic = new float[][] {new float[]{Mathf.Cos(beta) * Mathf.Cos(gamma), Mathf.Sin(alpha) * Mathf.Sin(beta) * Mathf.Cos(gamma) - Mathf.Cos(alpha) * Mathf.Sin(gamma), Mathf.Cos(alpha) * Mathf.Sin(beta) * Mathf.Cos(gamma) + Mathf.Sin(alpha) * Mathf.Sin(gamma)},
-    new float[]{Mathf.Cos(beta) * Mathf.Sin(gamma), Mathf.Sin(alpha) * Mathf.Sin(beta) * Mathf.Sin(gamma) + Mathf.Cos(alpha) * Mathf.Cos(gamma), Mathf.Cos(alpha) * Mathf.Sin(beta), Mathf.Sin(gamma) - Mathf.Sin(alpha) * Mathf.Cos(gamma)},
-    new float[]{-Mathf.Sin(beta), Mathf.Sin(alpha) * Mathf.Cos(beta), Mathf.Cos(alpha) * Mathf.Cos(beta)}};
-}
+        for (int i = 0; i < newMatrix.matrix.Length; i++)
+        {
+            for (int j = 0; j < newMatrix.matrix[i].Length; j++)
+            {
+                newMatrix.matrix[i][j] = SussyDotProduct(one.matrix[i], two.arrayFromColumn(j));
+            }
+        }
+
+        return newMatrix;
+    }
+
+    public float[] arrayFromColumn(int col)
+    {
+        float[] output = new float[matrix.Length];
+
+        for (int i = 0; i < output.Length; i++)
+        {
+            output[i] = matrix[i][col];
+        }
+
+        return output;
+    }
+
+    public static float SussyDotProduct(float[] one, float[] two)
+    {
+        float total = 0;
+        for (int i = 0; i < one.Length; i++) { total += one[i] * two[i]; }
+        return total;
+    }
+
+    public static Matrix RotationMatrix(Vector3 rad)
+    {
+        Matrix rotMat = new Matrix(ArrayHelper.FloatArray(3, 3));
+        float alpha = rad.z;
+        float beta = rad.y;
+        float gamma = rad.x;
+
+        rotMat.matrix[0][0] = Mathf.Cos(alpha) * Mathf.Cos(beta);
+        rotMat.matrix[0][1] = Mathf.Cos(alpha) * Mathf.Sin(beta) * Mathf.Sin(gamma) - (Mathf.Sin(alpha) * Mathf.Cos(gamma));
+        rotMat.matrix[0][2] = Mathf.Cos(alpha) * Mathf.Sin(beta) * Mathf.Cos(gamma) + (Mathf.Sin(alpha) * Mathf.Sin(gamma));
+
+        rotMat.matrix[1][0] = Mathf.Sin(alpha) * Mathf.Cos(beta);
+        rotMat.matrix[1][1] = Mathf.Sin(alpha) * Mathf.Sin(beta) * Mathf.Sin(gamma) + (Mathf.Cos(alpha) * Mathf.Cos(gamma));
+        rotMat.matrix[1][2] = Mathf.Sin(alpha) * Mathf.Sin(beta) * Mathf.Cos(gamma) - (Mathf.Cos(alpha) * Mathf.Sin(gamma));
+
+        rotMat.matrix[2][0] = -Mathf.Sin(beta);
+        rotMat.matrix[2][1] = Mathf.Cos(beta) * Mathf.Sin(gamma);
+        rotMat.matrix[2][2] = Mathf.Cos(beta) * Mathf.Cos(gamma);
+
+        return rotMat;
+    }
+
+    public static Matrix PositionMatrix(Vector3 pos)
+    {
+        float[][] sus = new float[][] { new float[] { pos.x }, new float[] { pos.y }, new float[] { pos.z } };
+        return new Matrix(sus);
+    }
+
+    public override string ToString()
+    {
+        string output = "";
+
+        for (int i = 0; i < matrix.Length; i++)
+        {
+            output += "[";
+            for (int j = 0; j < matrix[i].Length; j++)
+            {
+                output += matrix[i][j] + (j + 1 == matrix[i].Length ? "" : ", ");
+            }
+            output += "]\n";
+        }
+
+        return output;
+    }
+
+    public static explicit operator Vector3(Matrix matrix)
+    {
+        return new Vector3(matrix.matrix[0][0], matrix.matrix[1][0], matrix.matrix[2][0]);
+    }
 }
 
-/*float xPos = Mathff.Sin(transform.eulerAngles.y * Mathff.Deg2Rad) * Mathff.Cos(transform.eulerAngles.x * Mathff.Deg2Rad);
-        float yPos = Mathff.Sin(-transform.eulerAngles.x * Mathff.Deg2Rad);
-        float zPos = Mathff.Cos(transform.eulerAngles.x * Mathff.Deg2Rad) * Mathff.Cos(transform.eulerAngles.y * Mathff.Deg2Rad);
-*/
+public static class ArrayHelper
+{
+    public static float[][] FloatArray(int rows, int cols)
+    {
+        float[][] sus = new float[rows][];
+
+        for (int i = 0; i < sus.Length; i++)
+        {
+            sus[i] = new float[cols];
+        }
+
+        return sus;
+    }
+}
